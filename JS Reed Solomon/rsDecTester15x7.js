80393c894d2e96b7f95a93fb743d8b7434cbeba5
felippedr <felippe.ramos1@hotmail.com>
2016-09-14 18:21:53 -0300

    Decodificador JS
    Quase completo, mas só corrige 1 símbolo errado,  quando deveria
    corrigir 4.
    Faltam comentários.

--- JS Reed Solomon/rsDecTester15x7.js
+++ JS Reed Solomon/rsDecTester15x7.js
@@ -1,3 +1,41 @@
+
+function gfinvert(i){
+	var o;
+	if (i == "0000")
+		o = "0000";
+	else if (i == "0001")
+		o = "0001";
+	else if (i == "0010")
+		o = "1001";
+	else if (i == "0011")
+		o = "1110";
+	else if (i == "0100")
+		o = "1101";
+	else if (i == "0101")
+		o = "1011";
+	else if (i == "0110")
+		o = "0111";
+	else if (i == "0111")
+		o = "0110";
+	else if (i == "1000")
+		o = "1111";
+	else if (i == "1001")
+		o = "0010";
+	else if (i == "1010")
+		o = "1100";
+	else if (i == "1011")
+		o = "0101";
+	else if (i == "1100")
+		o = "1010";
+	else if (i == "1101")
+		o = "0100";
+	else if (i == "1110")
+		o = "0011";
+	else if (i == "1111")
+		o = "1000";
+	return o;
+}
+
 function gfmultiplication ( x, y){
 	var o;
 	if (x == "0000") { 
@@ -9,7 +47,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0010";}
  			if (y == "0011" ){ o = "0011";}
  			if (y == "0100" ){ o = "0100";}
- 			if (y == "1111" ){ o = "0101";}
+ 			if (y == "0101" ){ o = "0101";}
  			if (y == "0110" ){ o = "0110";}
  			if (y == "0111" ){ o = "0111";}
  			if (y == "1000" ){ o = "1000";}
@@ -29,7 +67,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0100";}
  			if (y == "0011" ){ o = "0110";}
  			if (y == "0100" ){ o = "1000";}
- 			if (y == "1111" ){ o = "1010";}
+ 			if (y == "0101" ){ o = "1010";}
  			if (y == "0110" ){ o = "1100";}
  			if (y == "0111" ){ o = "1110";}
  			if (y == "1000" ){ o = "0011";}
@@ -49,7 +87,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0110";}
  			if (y == "0011" ){ o = "0101";}
  			if (y == "0100" ){ o = "1100";}
- 			if (y == "1111" ){ o = "1111";}
+ 			if (y == "0101" ){ o = "1111";}
  			if (y == "0110" ){ o = "1010";}
  			if (y == "0111" ){ o = "1001";}
  			if (y == "1000" ){ o = "1011";}
@@ -69,7 +107,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1000";}
  			if (y == "0011" ){ o = "1100";}
  			if (y == "0100" ){ o = "0011";}
- 			if (y == "1111" ){ o = "0111";}
+ 			if (y == "0101" ){ o = "0111";}
  			if (y == "0110" ){ o = "1011";}
  			if (y == "0111" ){ o = "1111";}
  			if (y == "1000" ){ o = "0110";}
@@ -89,7 +127,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1010";}
  			if (y == "0011" ){ o = "1111";}
  			if (y == "0100" ){ o = "0111";}
- 			if (y == "1111" ){ o = "0010";}
+ 			if (y == "0101" ){ o = "0010";}
  			if (y == "0110" ){ o = "1101";}
  			if (y == "0111" ){ o = "1000";}
  			if (y == "1000" ){ o = "1110";}
@@ -109,7 +147,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1100";}
  			if (y == "0011" ){ o = "1010";}
  			if (y == "0100" ){ o = "1011";}
- 			if (y == "1111" ){ o = "1101";}
+ 			if (y == "0101" ){ o = "1101";}
  			if (y == "0110" ){ o = "0111";}
  			if (y == "0111" ){ o = "0001";}
  			if (y == "1000" ){ o = "0101";}
@@ -129,7 +167,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1110";}
  			if (y == "0011" ){ o = "1001";}
  			if (y == "0100" ){ o = "1111";}
- 			if (y == "1111" ){ o = "1000";}
+ 			if (y == "0101" ){ o = "1000";}
  			if (y == "0110" ){ o = "0001";}
  			if (y == "0111" ){ o = "0110";}
  			if (y == "1000" ){ o = "1101";}
@@ -149,7 +187,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0011";}
  			if (y == "0011" ){ o = "1011";}
  			if (y == "0100" ){ o = "0110";}
- 			if (y == "1111" ){ o = "1110";}
+ 			if (y == "0101" ){ o = "1110";}
  			if (y == "0110" ){ o = "0101";}
  			if (y == "0111" ){ o = "1101";}
  			if (y == "1000" ){ o = "1100";}
@@ -169,7 +207,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0001";}
  			if (y == "0011" ){ o = "1000";}
  			if (y == "0100" ){ o = "0010";}
- 			if (y == "1111" ){ o = "1011";}
+ 			if (y == "0101" ){ o = "1011";}
  			if (y == "0110" ){ o = "0011";}
  			if (y == "0111" ){ o = "1010";}
  			if (y == "1000" ){ o = "0100";}
@@ -189,7 +227,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0111";}
  			if (y == "0011" ){ o = "1101";}
  			if (y == "0100" ){ o = "1110";}
- 			if (y == "1111" ){ o = "0100";}
+ 			if (y == "0101" ){ o = "0100";}
  			if (y == "0110" ){ o = "1001";}
  			if (y == "0111" ){ o = "0011";}
  			if (y == "1000" ){ o = "1111";}
@@ -209,7 +247,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "0101";}
  			if (y == "0011" ){ o = "1110";}
  			if (y == "0100" ){ o = "1010";}
- 			if (y == "1111" ){ o = "0001";}
+ 			if (y == "0101" ){ o = "0001";}
  			if (y == "0110" ){ o = "1111";}
  			if (y == "0111" ){ o = "0100";}
  			if (y == "1000" ){ o = "0111";}
@@ -229,7 +267,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1011";}
  			if (y == "0011" ){ o = "0111";}
  			if (y == "0100" ){ o = "0101";}
- 			if (y == "1111" ){ o = "1001";}
+ 			if (y == "0101" ){ o = "1001";}
  			if (y == "0110" ){ o = "1110";}
  			if (y == "0111" ){ o = "0010";}
  			if (y == "1000" ){ o = "1010";}
@@ -249,7 +287,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1001";}
  			if (y == "0011" ){ o = "0100";}
  			if (y == "0100" ){ o = "0001";}
- 			if (y == "1111" ){ o = "1100";}
+ 			if (y == "0101" ){ o = "1100";}
  			if (y == "0110" ){ o = "1000";}
  			if (y == "0111" ){ o = "0101";}
  			if (y == "1000" ){ o = "0010";}
@@ -269,7 +307,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1111";}
  			if (y == "0011" ){ o = "0001";}
  			if (y == "0100" ){ o = "1101";}
- 			if (y == "1111" ){ o = "0011";}
+ 			if (y == "0101" ){ o = "0011";}
  			if (y == "0110" ){ o = "0010";}
  			if (y == "0111" ){ o = "1100";}
  			if (y == "1000" ){ o = "1001";}
@@ -289,7 +327,7 @@ function gfmultiplication ( x, y){
  			if (y == "0010" ){ o = "1101";}
  			if (y == "0011" ){ o = "0010";}
  			if (y == "0100" ){ o = "1001";}
- 			if (y == "1111" ){ o = "0110";}
+ 			if (y == "0101" ){ o = "0110";}
  			if (y == "0110" ){ o = "0100";}
  			if (y == "0111" ){ o = "1011";}
  			if (y == "1000" ){ o = "0001";}
@@ -319,7 +357,7 @@ function xor (x, y){
 	return output;
 }
 
-var input = ["1000", "0010", "1000", "0010", "1000", "0010", "1000"];
+var input = ["1000", "0010", "1000", "0010", "1000", "0010", "1010"];
 var reg1 = "0000";
 var reg2 = "0000";
 var reg3 = "0000";
@@ -375,7 +413,9 @@ for (var i = 0; i < 7; i ++){
 
 console.log(reg1+","+reg2+","+reg3+","+reg4+","+reg5+","+reg6+","+reg7+","+reg8);
 
-var input2 = ["1000", "0010", "1000", "0010", "1000", "0010", "1000", reg8, reg7, reg6, reg5, reg4, reg3, reg2, reg1];
+//var input = ["1000", "0010", "1000", "0010", "1000", "0010", "1010"];
+ var input2 = ["1010", "0010", "1000", "0010", "1000", "0010", "1010", reg8, reg7, reg6, reg5, reg4, reg3, reg2, reg1];
+
 
 
 for(var j = 0; j <= 14; j++){
@@ -389,4 +429,193 @@ for(var j = 0; j <= 14; j++){
 	syn7 = xor(input2[j], gfmultiplication(syn7, "1011"));	
 }
 
-console.log(syn0+","+syn1+","+syn2+","+syn3+","+syn4+","+syn5+","+syn6+","+syn7);
+console.log("Sindromes: "+syn0+","+syn1+","+syn2+","+syn3+","+syn4+","+syn5+","+syn6+","+syn7);
+
+var sreg0 = "0000";
+var sreg1 = "0000";
+var sreg2 = "0000";
+var sreg3 = "0000";
+var sreg4 = "0000";
+
+var creg0 = "0000";
+var creg1 = "0000";
+var creg2 = "0000";
+var creg3 = "0000";
+
+var breg0 = "0000";
+var breg1 = "0000";
+var breg2 = "0000";
+var breg3 = "0000";
+
+var creg0a = "0000";
+var creg1a = "0000";
+var creg2a = "0000";
+var creg3a = "0000";
+
+var breg0a = "0000";
+var breg1a = "0000";
+var breg2a = "0000";
+var breg3a = "0000";
+
+var regdn = "0000";
+
+var regdi = "0000";
+
+var regaux = "0000";
+
+syndrome = [syn0, syn1, syn2, syn3, syn4, syn5, syn6, syn7];
+
+for(var k = 0; k < 8; k++){
+	sreg0 = syndrome[k];
+	
+	regdn = xor(
+					sreg0, 
+					xor(
+						xor(
+							gfmultiplication(sreg1, creg0), 
+							gfmultiplication(sreg2, creg1)
+						), 
+						xor(
+							gfmultiplication(sreg3, creg2),
+							gfmultiplication(sreg4, creg3)
+						)
+					)
+				);
+	
+	regaux = gfmultiplication(regdi, regdn);
+	
+	creg0a = xor(creg0, gfmultiplication(breg0, regaux));
+	creg1a = xor(creg1, gfmultiplication(breg1, regaux));
+	creg2a = xor(creg2, gfmultiplication(breg2, regaux));
+	creg3a = xor(creg3, gfmultiplication(breg3, regaux));
+	
+	if(regdn == "0000" || l%2 == 1){
+		breg0a = breg3;
+		breg1a = breg0;
+		breg2a = breg1;
+		breg3a = breg2;
+	}
+	else{
+		regdi = gfinvert(regdn);
+		breg0a = "0001";
+		breg1a = creg0;
+		breg2a = creg1;
+		breg3a = creg2; 
+	}
+	
+	breg0 = breg0a;
+	breg1 = breg1a;
+	breg2 = breg2a;
+	breg3 = breg3a;
+	
+	creg0 = creg0a;
+	creg1 = creg1a;
+	creg2 = creg2a;
+	creg3 = creg3a;
+	
+	sreg1 = sreg0;
+	sreg2 = sreg1;
+	sreg3 = sreg2;
+	sreg4 = sreg3;
+	
+}
+
+var lambda = [creg0, creg1, creg2, creg3];
+
+var omega = ["0000", "0000", "0000", "0000"];
+
+sreg0 = "0000";
+sreg1 = "0000";
+sreg2 = "0000";
+sreg3 = "0000";
+sreg4 = "0000";
+
+for(var l = 0; l < 4; l++){
+	sreg0 = syndrome[l];
+	
+	omega[l] = xor(
+					sreg0, 
+					xor(
+						xor(
+							gfmultiplication(sreg1, creg0), 
+							gfmultiplication(sreg2, creg1)
+						), 
+						xor(
+							gfmultiplication(sreg3, creg2),
+							gfmultiplication(sreg4, creg3)
+						)
+					)
+				);
+				
+				
+	sreg1 = sreg0;
+	sreg2 = sreg1;
+	sreg3 = sreg2;
+	sreg4 = sreg3;	
+}
+
+console.log("Lambda: " + lambda);
+
+console.log("Omega: " + omega);
+
+
+var chienLocR = "0000";
+var chienLocRO = "0000";
+var chienVal = "0000";
+
+var csl0 = lambda[0];
+var csl1 = lambda[1];
+var csl2 = lambda[2];
+var csl3 = lambda[3];
+
+var csv0 = omega[0];
+var csv1 = omega[1];
+var csv2 = omega[2];
+var csv3 = omega[3];
+
+
+for(var m = 0; m < 7; m++){
+	csl0 = gfmultiplication(csl0, "0010");
+	csl1 = gfmultiplication(csl1, "0100");
+	csl2 = gfmultiplication(csl2, "0010");
+	csl3 = gfmultiplication(csl3, "1000");
+	
+	chienLocRO = xor(
+		csl0,
+		csl2
+	);
+	
+	chienLocR = xor(
+		xor(csl1, csl3),
+        xor("0001", chienLocRO)
+	);
+	
+	csv1 = gfmultiplication(csv1, "0010");
+	csv2 = gfmultiplication(csv2, "0100");
+	csv3 = gfmultiplication(csv3, "1000");
+	
+	chienVal = xor(
+		xor(csv0, csv2),
+		xor(csv1, csv3)
+	);
+	
+	chienLocRO = gfinvert(chienLocRO);
+	
+	console.log("Chien Mult "+ m +": "+ gfmultiplication(chienVal, chienLocRO));
+	console.log("Chien Loc "+ m +": "+ chienLocR);
+	
+	if(chienLocR == "0000")
+		input2[m] = xor(input2[m], gfmultiplication(chienVal, chienLocRO));
+	
+}
+
+console.log(input2);
+
+
+
+
+
+
+
+
+
